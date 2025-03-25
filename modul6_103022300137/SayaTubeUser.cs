5b767be1abbdef6f23fc65411c47a63b4d9720a5
gmii <gumilarharisubagja28@gmail.com>
2025-03-25 08:03:36 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_103022300137/SayaTubeUser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300137
+{
+    class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> videos;
+        public string username;
+
+        public SayaTubeUser(string username)
+        {
+            if (username == null || username.Length > 100)
+            {
+                throw new ArgumentException("Username tidak boleh kosong dan tidak boleh lebih dari 50 karakter");
+            }
+            Random random = new Random();
+            this.id = random.Next(1000, 9999);
+            this.username = username;
+            this.videos = new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int totalPlayCount = 0;
+            foreach (SayaTubeVideo video in videos)
+            {
+                totalPlayCount += video.GetPlayCount();
+            }
+            return totalPlayCount;
+        }
+
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            if (video == null)
+            {
+                throw new ArgumentException("Video tidak boleh kosong");
+            }
+            videos.Add(video);
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("Total play count for user " + username + ": ");
+            for(int i = 0; i < Math.Min(videos.Count, 8); i++)
+            {
+                Console.WriteLine($"Video {i+1} judul: {videos[i].getTitle()}");
+            }
+        }
+    }
+}
---
+++ modul6_103022300137/SayaTubeVideo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300137
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (title == null || title.Length > 200) 
+            {
+                throw new ArgumentException("Judul tidak boleh kosong dan tidak boleh lebih dari 200 karakter");
+            }
+            Random random = new Random();
+            this.id = random.Next(1000, 9999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void increasePlayCount(int count)
+        {
+            if (count < 0 || count > 25000000)
+            {
+                throw new ArgumentException("Invalid play count: error increment");
+            }
+
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                throw new ArgumentException("Invalid play count: Play count overflow");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Video Title: " + title);
+            Console.WriteLine("Video Play Count: " + playCount);
+        }
+
+        public int GetPlayCount()
+        {
+            return playCount;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+    }
+}
